3f9a9bcbc88d9e81e84fa401139e663170e25a01
Jeremy Alosio <jeremy.alosio@gmail.com>
2020-07-24 13:10:17 -0400

    Changed to new page style

--- src/App.js
+++ src/App.js
@@ -1,43 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import Sidebar from "./Sidebar"
-import GrayoutCurtain from './Objects/GrayoutCurtain';
-import DrawerToggleButton from './Objects/DrawerToggleButton';
+import HighlightingTextButton from './Objects/HighlightingTextButton';
+import AnimatedBackground from './Objects/AnimatedBackground';
 
 class App extends Component {
-
-  state = {
-    sideDrawerOpen: false
-  };
-
-  drawerToggleClickHandler = () => {
-    this.setState((prevState) => {
-      return { sideDrawerOpen: !prevState.sideDrawerOpen }
-    });
-  };
-
-  backdropClickHandler = () => {
-    this.setState({sideDrawerOpen: false});
-  }
-
   render() {
-    let backdrop;
-
-    if (this.state.sideDrawerOpen) {
-      backdrop = <GrayoutCurtain click={this.backdropClickHandler}/>
-    }
-
     return (
       <div className="App">
-        <div className="hamburger">
-          <DrawerToggleButton click={this.drawerToggleClickHandler}/>
+        <div className="background">
+        <AnimatedBackground />
+          <div className="title_text">
+            <h1>Hey, I'm Jeremy Alosio. <br /> I like to code.</h1>
+            <HighlightingTextButton />
+          </div>
         </div>
-        {backdrop}
-        <Sidebar show={this.state.sideDrawerOpen}/>
-        
 
-        
-        <h1>HELLO REACT!</h1>
       </div>
     );
   }
